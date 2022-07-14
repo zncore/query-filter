a732cf12129407910b26521f1a376d8685e6b006
Vitaliy <theyamshikov@yandex.ru>
2022-07-14 09:57:10 +0600

    upd

--- src/Interfaces/DefaultSortInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZnCore\QueryFilter\Interfaces;
-
-interface DefaultSortInterface
-{
-
-    public function defaultSort(): array;
-}
--- src/Interfaces/IgnoreAttributesInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZnCore\QueryFilter\Interfaces;
-
-interface IgnoreAttributesInterface
-{
-
-    public function ignoreAttributesFromCondition(): array;
-}
